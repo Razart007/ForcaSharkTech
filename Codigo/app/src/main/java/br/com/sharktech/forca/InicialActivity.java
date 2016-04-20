3ef878773a402aa1a2f5f9ed368b6187deb091b3
Rafael <rafael.redsky@gmail.com>
2016-04-19 22:16:11 -0300

    Primeiro Commit, com a primeira versão do código.

---
+++ Codigo/app/src/main/java/br/com/sharktech/forca/InicialActivity.java
@@ -0,0 +1,17 @@
+package br.com.sharktech.forca;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class InicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+    }
+}
---
+++ Codigo/app/src/main/java/br/com/sharktech/forca/SplashActivity.java
@@ -0,0 +1,29 @@
+package br.com.sharktech.forca;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+
+                Intent intent = new Intent();
+                intent.setClass(SplashActivity.this, InicialActivity.class);
+                startActivity(intent);
+            }
+        }, 6000);
+    }
+}
