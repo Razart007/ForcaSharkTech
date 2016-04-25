0df29e797bf5c36061b9371e88c907829568236b
Rafael <rafael.redsky@gmail.com>
2016-04-25 17:03:42 -0300

    Modificação e correção da tela de categoria!

--- Codigo/app/src/main/java/br/com/sharktech/forca/InicialActivity.java
+++ Codigo/app/src/main/java/br/com/sharktech/forca/InicialActivity.java
@@ -1,17 +1,49 @@
 package br.com.sharktech.forca;
 
+import android.annotation.TargetApi;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class InicialActivity extends AppCompatActivity {
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+/*
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.gallows, "Exemplo", "Exemplo1") {
+            public void onDrawerClosed(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    getActionBar().setTitle("Exemplo1");
+                }
+            }
+
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle("ooo");
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+   */
     }
 }
